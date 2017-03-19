9df012bce22afc8b6c59fc3fd12b0bb74bf74878
zryzhangruyi <731134543@qq.com>
2017-03-19 21:42:30 +0800

    Signed-off-by: zryzhangruyi <731134543@qq.com>

---
+++ Downlod/server/js/utils/readFile.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var fs = require('fs');
+/**
+ * 读取网页并返回
+ * @param path 网页所在路径
+ */
+function readHTML(path,response) {
+    fs.readFile(path, function (err, data) {
+        if (err) {
+            console.log(err);
+            // HTTP 状态码: 404 : NOT FOUND
+            response.writeHead(404, {'Content-Type': 'text/html'});
+        } else {
+            // HTTP 状态码: 200 : OK
+            response.writeHead(200, {'Content-Type': 'text/html'});
+            // 响应文件内容
+            response.write(data.toString());
+            response.end();
+        }
+    });
+}
+exports.readHTML=readHTML;
